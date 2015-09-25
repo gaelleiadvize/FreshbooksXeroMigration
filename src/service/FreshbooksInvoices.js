89555e5fe29638fbb74be23c1dc956051605ad41
gaelleiadvize <gaelle.acas@iadvize.com>
2015-09-25 17:54:16 +0200

    check Credit notes

--- src/service/FreshbooksInvoices.js
+++ src/service/FreshbooksInvoices.js
@@ -2,6 +2,7 @@
 
 var when = require('when');
 var traverse = require('traverse');
+var moment = require('moment');
 
 
 module.exports = function(Freshbooks, Xero, logger) {
@@ -18,7 +19,7 @@ module.exports = function(Freshbooks, Xero, logger) {
 
 		var deferred = when.defer();
 
-		invoiceApi.list({status: status, per_page : 100, page: page},function(err, invoices, options) {
+		invoiceApi.list({status: 'paid', number: 'AQ460' ,per_page : 100, page: page},function(err, invoices, options) {
 			logger.info('Invoices page : ' + page);
 			if (err) {
 
@@ -46,82 +47,170 @@ module.exports = function(Freshbooks, Xero, logger) {
 	}
 
 	function setApprouved(invoices) {
-		
-		//var data = [];
-		var deferred = when.defer();
 
-		when.map(invoices, function (invoice){
+		return when.map(invoices, function (invoice){
 			return {
 				InvoiceNumber : invoice.number,
 				Status : 'AUTHORISED'
 			}
 		})
-		//.then(JSON.stringify)
-		.then(callApprouved)
-		.then(function (result){
-			//logger.info(result);
-		});
-
-
-		var data = invoices.map(function (invoice){
-			return {
-				InvoiceNumber : invoice.number,
-				Status : 'AUTHORISED'
-			}
-		});
-
-		// _.forEach(invoices, function (invoice){
-		// 	data.push({
-		// 		InvoiceNumber : invoice.number,
-		// 		Status : 'AUTHORISED'
-		// 	});
+		.then(callApprouved);
+		// .then(function (data){
+		// 	return when.resolve(invoices);
+		// }).catch(function(err){
+		// 	logger.error('erreur');
 		// });
-		
-		deferred.resolve({
-			approuved : data,
-			invoices : invoices
-		});
-		logger.info(_.size(data) + ' invoices approuved'); 
 
-		return deferred.promise;
 	}
 
 	function callApprouved(data){
 
-		return when.promise(function (resolve, reject) {
-			 	logger.info('calling api xero for approuved ....')
-				Xero.call('POST', '/Invoices/?SummarizeErrors=false', data, function(err, json) {
-					if (err) {
-				    	logger.error('NOT APPROUVED');
-				    	return reject(err); 
-					} else {
-						logger.info(_.size(data) + ' APPROUVED DONE !!!!!!!!!!');
+		var deferred = when.defer();
 
-						when(json.Response)
-						//.then(JSON.stringify)
-						.then(function (items){
-							var InvoicesError = [];
-							traverse(items).forEach(function (item) {
-								if ('ValidationErrors' == this.key){
-									InvoicesError.push(this.parent.node.InvoiceNumber);
-								}								
-							});
-							logger.error(InvoicesError);
-						});
-						return resolve(invoiceList); 
-					}
+		logger.info('calling api xero for approuved ....')
+		Xero.call('POST', '/Invoices/?SummarizeErrors=false', data, function(err, json) {
+			if (err) {
+		    	logger.error('NOT APPROUVED');
+		    	deferred.reject(err);
+			} else {
+				
+				var InvoicesError = [];
+				when(json.Response)
+				//.then(JSON.stringify)
+				.then(function (items){					
+					traverse(items).forEach(function (item) {
+						if ('ValidationErrors' == this.key){
+							InvoicesError.push(this.parent.node.InvoiceNumber);
+						}								
+					});
+					logger.error(InvoicesError);
+					var total = _.size(data);
+					var errors = _.size(InvoicesError);
+					logger.info((total - errors) + ' APPROUVED DONE !!!!!!!!!!');
+
+					return deferred.resolve(invoiceList);
 				});
 			}
-		);
+		});
+
+		return deferred.promise;
+
 	}
+
+	function addPayments(invoices){
+		return when(invoices)
+				.then(addPayment)
+				.then(callPayments);
+	}
+
+	function getPayment (invoice) {
+	    var deferred = when.defer();
+
+		paymentApi.list({invoice_id: invoice.id},function(err, payments, options) {
+			if (err) {
+				logger.error(err);
+				deferred.reject(err); 
+			} else {
+
+				logger.info(payments);
+				var paymentList = [];
+				_.forEach(payments, function (payment){
+					var paid = {
+						Invoice : {
+							InvoiceNumber : invoice.number	
+						},			
+						Amount : payment.amount,
+						Account : {
+							AccountID : '5c0bcc56-99a1-464d-ac45-defe718131ee'
+						},
+						Date : moment(payment.date).format('YYYY-MM-DD'),
+						Reference : payment.type + ' - ' + payment.notes
+						}
+					paymentList.push(paid);
+
+					if (payment.type == 'Credit') {
+						//setStatusPaid(payment.notes);
+					}
+				});
+				deferred.resolve(paymentList); 
+			}
+		});
+
+	return deferred.promise;
+}
+
+function setStatusPaid(invoiceID){
+	var data = [{
+		Type : 'ACCRECCREDIT',
+		InvoiceNumber : invoiceID,
+		Status : 'PAID'
+	}];
+
+	Xero.call('POST', '/creditnotes/', data, function(err, json) {
+		if (err) {
+			logger.error(err);
+		} else {
+			logger.info('Credit note created with success ')
+		}
+	});
+
+
+}
+
+function addPayment(invoices) {
+	//875b9c4e-5715-45b4-a120-bd615397b1fc
+	logger.info('Adding payments .....');
+	 var deferreds = [];
+	 _.forEach(invoices, function (invoice){
+	 	deferreds.push(getPayment(invoice));
+	 });
+	
+	return when.all(deferreds).then(format);
+}
+
+function format(data){
+	logger.info('Formating data ....');
+
+	var deferred = when.defer();
+	var paymentsList = [];
+	_.forEach(data, function (payments) {
+		_.forEach(payments, function (payment) {
+			paymentsList.push(payment);	
+		});
+	});
+
+	return when.all(paymentsList);
+}
+
+
+function callPayments(data){
+
+	var deferred = when.defer();
+	logger.info('Call payment API');
+	Xero.call('POST', '/Payments/?SummarizeErrors=false', data, function(err, json) {
+		if(err) {
+			logger.error(err);
+			deferred.reject(err);
+		} else {
+			logger.info(_.size(data) + ' payments DONE !!!!');
+			deferred.resolve(json);
+		}
+	});
+
+	return deferred.promise;
+}
 
 	return {
 		paymentMigration: function (type, page){
 			listInvoices(type, page)
 			.then(setApprouved)
+			.then(addPayments)
 			.then(function (data){
 				//logger.info(data);
-			})
+				//logger.info(data);
+			}).catch(function (err){
+				logger.error(err);
+			});
 		}
 	}
 }
