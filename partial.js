3f85f5b55c7e90290699d65759887257d28d0337
gaelleiadvize <gaelle.acas@iadvize.com>
2015-10-12 17:44:03 +0200

    add partial scripts

--- partial.js
+++ partial.js
@@ -5,17 +5,21 @@ require('./bootstrap');
 var logger = require('./helper/logger');
 var config = require('./config/config')(logger);
 var when = require('when');
-var moment = require('moment');
-
 
 var FreshBooks = require("./helper/freshbooks")(config.freshbooks, logger);
 var Xero = require("./helper/xero")(config.xero, logger);
+var Cache = require("./helper/cache")('partial', logger);
 
-var FreshbooksInvoices = require('./src/service/FreshbooksInvoices')(FreshBooks, Xero, logger);
 
-var invoice = new FreshBooks.Invoice();
-var payment = new FreshBooks.Payment;
+var FreshbooksApi = require('./src/service/FreshbooksApi')(FreshBooks, Cache, logger);
+var XeroApi = require('./src/service/XeroApi')(Xero, Cache, logger);
 
-var invoiceList = [];
-FreshbooksInvoices.paymentMigration('partial', 1);
+//var FreshbooksInvoices = require('./src/service/FreshbooksInvoices')(FreshBooks, Xero, logger);
 
+var Migration = require('./src/service/Migration')(FreshbooksApi, XeroApi, logger);
+
+//logger.debug(Cache.getInstance().keys('toto'));
+
+
+//FreshbooksInvoices.paymentMigration('paid', 1);
+Migration.paymentMigration('partial', 1);
